74124b571b2fbd284d20cd380ce1a5ffd652170d
PratimaGurav <prtmgrv2@gmail.com>
2021-11-07 18:30:53 +0000

    Removed trailing whitespace

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import string
 from words import words
 from hangman_display import display_hangman
 
+
 class tcolors:
     GREEN = '\033[92m'
     RED = '\033[91m'
@@ -14,7 +15,7 @@ class tcolors:
 
 def title_msg():
     """
-    Function for title 
+    Function for title
     """
     print(
         """
@@ -24,9 +25,7 @@ def title_msg():
         |  _  | (_| | | | | (_| | | | | | | (_| | | | |
         |_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
                             |___/
-        """
-    )
-    print(tcolors.BLUE +
+        """+ tcolors.BLUE +
         """
         How to play:
        * Enter username to start the game.
@@ -36,8 +35,7 @@ def title_msg():
        * Game is over either when you have correctly guessed the word
        * Or when you have run out of lives.
        * Good Luck!
-        """
-        + tcolors.WHITE
+        """+ tcolors.WHITE
     )
 
 
@@ -51,16 +49,18 @@ def start_game():
 
     name = input("Please enter your name:\n>")
     if name == "":
-        print(tcolors.WARNING + "You need to enter a username to continue...\n>" + tcolors.WHITE)
+        print(tcolors.WARNING + "You need to enter a username to continue...\n>" +
+            tcolors.WHITE)
         start_game()
     else:
        print(f"Hello, {name}")
-       select_difficulty()        
+       select_difficulty()
+
 
 def select_difficulty():
     """
     Let player set difficulty
-    """ 
+    """
     selected_difficulty = input("Select difficulty level\n"
         "Please enter E for Easy or H for Hard:\n>")
     selected_difficulty = selected_difficulty.upper()
@@ -72,8 +72,8 @@ def select_difficulty():
         lives = 7
         hangman(lives)
     else:
-        print(tcolors.WARNING + "You must select E or H. Please try again\n"
-            + tcolors.WHITE)
+        print(tcolors.WARNING + "You must select E or H. Please try again\n"+
+            tcolors.WHITE)
         select_difficulty()
 
 
@@ -114,35 +114,37 @@ def hangman(lives):
 
             else:
                 lives = lives - 1  # takes away a life if wrong
-                print(tcolors.WARNING + 'Letter is not in word.'
-                    + tcolors.WHITE)
+                print(tcolors.WARNING + "Letter is not in word."+
+                    tcolors.WHITE)
 
         elif user_letter in used_letters:
-            print(tcolors.WARNING + "You have already used that character." 
+            print(tcolors.WARNING + "You have already used that character."
                 "Please try again." + tcolors.WHITE)
 
         else:
-            print(tcolors.WARNING + 'Invalid letter. Please try again.' 
-                + tcolors.WHITE)
+            print(tcolors.WARNING + 'Invalid letter. Please try again.'+
+                 tcolors.WHITE)
 
     # gets here when len(word_letters) == 0 or when lives == 0
     if lives == 0:
         print(display_hangman[remaining_lives])
-        print(tcolors.RED + 'Game Over! The correct word was', word 
-            + tcolors.WHITE)
+        print(tcolors.RED + 'Game Over! The correct word was', word +
+            tcolors.WHITE)
         restart_game()
     else:
-        print(tcolors.GREEN + 'You have correctly guessed the word', word, '\n Congratulations!!' + tcolors.WHITE)
+        print(tcolors.GREEN + 'You have correctly guessed the word', word,
+            '\n Congratulations!!' + tcolors.WHITE)
         restart_game()
 
 
 def restart_game():
-    """ Gives player an option to restart, otherwise returns to title screen 
+    """ Gives player an option to restart, otherwise returns to title screen
     """
     game_restart = False
 
     while not game_restart:
-        restart = input('Would you like to play Hangman again? (Y/N)\n').upper()
+        restart = input(tcolors.BOLD + "Would you like to play Hangman again?"
+            "(Y/N)\n").upper()
 
         if restart == "Y":
             game_restart = True
@@ -150,11 +152,11 @@ def restart_game():
 
         elif restart == "N":
             game_restart = True
-            print(tcolors.BLUE + 'Thank You for playing Hangman-CoC. Goodbye!'
-                + tcolors.WHITE)
+            print(tcolors.BLUE + "Thank You for playing Hangman-CoC."
+                "Goodbye!" + tcolors.WHITE)
         else:
-            print(tcolors.WARNING + 'You must select Y or N. Please try again.'
-                + tcolors.WHITE)
+            print(tcolors.WARNING + "You must select Y or N."
+                "Please try again." + tcolors.WHITE)
 
 if __name__ == "__main__":
     start_game()
